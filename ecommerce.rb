77f904815e82868f355208df0f09b89309325f72
Alireza Andersen <alireza.andersen@gmail.com>
2018-08-20 17:18:59 -0600

    more attempts

--- ecommerce.rb
+++ ecommerce.rb
@@ -4,18 +4,21 @@ require 'bigdecimal'
 class Ecommerce
 
   def self.totals?(prices)
-    
-    # arr = []
-    # prices.each do |v|
+    arr = []
+    prices.each do |v|
     #   if v.to_s.chars
+    # arr << v.round(2)
+
+    arr << "0."+ "#{v.to_s.split('.').last.chars.map{ |d| d.to_i}.sum}"
     #     binding.pry
     #     # .last.to_i >= 4
     #     arr << v.ceil
     #   else
     #     arr << v.floor
     #   end
-    # end
-    # arr
+    end
+    binding.pry
+    arr.map {|a| a.to_f }.sum.round
   end
 
 end
