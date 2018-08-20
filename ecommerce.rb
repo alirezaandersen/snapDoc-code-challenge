893e42ba78c4d343d7c667c6838c972a24ac1ee3
Alireza Andersen <alireza.andersen@gmail.com>
2018-08-20 15:58:22 -0600

    still not working

--- ecommerce.rb
+++ ecommerce.rb
@@ -1,9 +1,21 @@
 require 'pry'
+require 'bigdecimal'
 
 class Ecommerce
 
   def self.totals?(prices)
-
+    
+    # arr = []
+    # prices.each do |v|
+    #   if v.to_s.chars
+    #     binding.pry
+    #     # .last.to_i >= 4
+    #     arr << v.ceil
+    #   else
+    #     arr << v.floor
+    #   end
+    # end
+    # arr
   end
-  
+
 end
